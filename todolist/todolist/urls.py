c1aecb28d4f7e9f940680cbfafc99180d185d958
pse <pedchenko.sergey@gmail.com>
2022-03-12 10:31:13 +0300

    created swagger/redoc documentation

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -1,13 +1,30 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from userapp.views import UserPortalModelViewSet
 from projectapp.views import ProjectModelViewSet, TodoModelViewSet
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ToDoApplication",
+        default_version='1',
+        description="Documentation to API to work with task manager",
+        contact=openapi.Contact(email="admin@admin.local"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 router = DefaultRouter()
 router.register('users', UserPortalModelViewSet)
@@ -22,6 +39,10 @@ urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger<str:format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 
 ]
