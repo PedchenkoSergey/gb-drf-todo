16cbb4dade56929720a77d4d39f7ced1d688ad3d
spedchenko <se.pedchenko@yandex.ru>
2022-04-18 14:13:16 +0300

    created docker-compose for db, adminer, drf. corrected fixtures.json

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -89,9 +89,17 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tdlist',
+        'USER': 'tdlist',
+        'PASSWORD': 'tdlist_password',
+        'HOST': 'db',
+        'POST': '5432',
     }
 }
 
